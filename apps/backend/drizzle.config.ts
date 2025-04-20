e55d36942f1584bc9d88ddc371632ff4943801a4
Harsh Mahadev Duche <ducheharsh@gmail.com>
2025-04-20 13:10:42 +0530

    fix: build issues

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,16 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
-if (!process.env.DATABASE_URL || !process.env.DATABASE_AUTH_TOKEN) {
-  throw new Error('Database credentials are missing in environment variables');
-}
-
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
-    authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: process.env.DATABASE_URL ?? "",
+    authToken: process.env.DATABASE_AUTH_TOKEN ?? "",
   },
 });
