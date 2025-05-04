535bb0b93251519af1de4b54094d047f4913771d
Harsh-Duche <ducheharsh@gmail.com>
2025-05-04 21:41:58 +0530

    fix: minor issues

--- apps/frontend/src/lib/s3/cloudflareR2Service.ts
+++ apps/frontend/src/lib/s3/cloudflareR2Service.ts
@@ -1,37 +1,21 @@
-import { cloudflareR2ConfigSchema } from "@/types/lib";
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { env } from "../config/env";
 
 export class CloudflareR2Service {
-  private input = cloudflareR2ConfigSchema.safeParse({
-    accountId: env.R2_ACCOUNT_ID,
-    accessKeyId: env.R2_ACCESS_KEY_ID,
-    secretAccessKey: env.R2_SECRET_ACCESS_KEY,
-    bucketName: env.R2_BUCKET_NAME,
-  });
-  accountId = this.input.success ? this.input.data.accountId : "";
-  bucketName = this.input.success ? this.input.data.bucketName : "";
-  private accessKeyId = this.input.success ? this.input.data.accessKeyId : "";
-  private secretAccessKey = this.input.success
-    ? this.input.data.secretAccessKey
-    : "";
   private region = "auto";
+  public accountId = env.R2_ACCOUNT_ID;
+  public bucketName = env.R2_BUCKET_NAME;
 
-  private S3 = () => {
-    if (!this.input.success) {
-      throw new Error("R2 credentials are not properly configured");
-    }
-
-    return new S3Client({
+  private S3 = () =>
+    new S3Client({
       endpoint: `https://${this.accountId}.r2.cloudflarestorage.com`,
       credentials: {
-        accessKeyId: this.accessKeyId,
-        secretAccessKey: this.secretAccessKey,
+        accessKeyId: env.R2_ACCESS_KEY_ID,
+        secretAccessKey: env.R2_SECRET_ACCESS_KEY,
       },
       region: this.region,
     });
-  };
 
   public async getSignedUrlForUpload(
     key: string,
